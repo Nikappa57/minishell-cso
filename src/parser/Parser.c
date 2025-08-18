7e40a4abfc014cb51ba88c9419ec29689e46b0a5
Nikappa57 <lorenz.gaudino@gmail.com>
2025-08-19 01:14:27 +0200

    Parser class created

---
+++ src/parser/Parser.c
@@ -0,0 +1,21 @@
+#include "Minishell.h"
+
+void Parser_init(ListHead *p) {
+	List_init(p);
+}
+
+void Parser_clear(ListHead *p) {
+	if (!p->size || !p->first) {
+		return;
+	}
+
+	ListItem* aux = p->first;
+	while (aux) {
+		Command* c_item = (Command*)(aux);
+		assert(c_item && "Parser_clear | invalid token cast");
+		aux = aux->next;
+		Command_free(c_item);    // free allocated text
+		free(c_item);          // free token struct
+	}
+	List_init(p);
+}
