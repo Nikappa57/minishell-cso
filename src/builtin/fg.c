7baa9b099053457d84194bef299e295fc5da8dcc
Nikappa57 <lorenz.gaudino@gmail.com>
2025-09-29 16:43:38 +0200

    Fix error in fg for consistency

--- src/builtin/fg.c
+++ src/builtin/fg.c
@@ -4,15 +4,15 @@
 int ft_fg(Executor *e, Command *cmd) {
 	// check if shell is interactive
 	if (!e->interactive)
-		return (error(1, "jobs: fg: no job control"), 0);
+		return (error(1, "fg: no job control"), 0);
 
 	// find job by arg (the default is current job)
 	char *str_idx = (cmd->argc == 1) ? "+" : cmd->argv[1];
 	Job *j = Executor_jobs_get(e, str_idx);
 
 	// job not found
-	if (!j)
-		return (error(1, "jobs: %s: no such job",
+	if (!j || j->state == JOB_DONE)
+		return (error(1, "fg: %s: no such job",
 			!strcmp(str_idx, "+") ? "current" : str_idx), 0);
 	
 	// give terminal to job
@@ -27,7 +27,12 @@ int ft_fg(Executor *e, Command *cmd) {
 		// man: If pid is less than -1, then sig is sent to every
 		// process in the process group whose ID is -pid.
 		int ret = kill(-j->pgid, SIGCONT);
-		if (ret == -1) perror("fg: SIGCONT");
+		if (ret == -1) {
+			// give terminal to shell
+			give_terminal_to(e->shell_pgid, e->tty_fd);
+			error(1, "fg: SIGCONT: %s", strerror(errno));
+			return (1);
+		}
 	}
 
 	// wait for the job
