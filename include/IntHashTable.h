e1f59e9d3f140485bcb1da4993bda2baa824fcef
Nikappa57 <lorenz.gaudino@gmail.com>
2025-10-03 14:50:27 +0200

    IntHashTable created
    simple hash table for int value and key
    methods: init, clear, add, get, remove

---
+++ include/IntHashTable.h
@@ -0,0 +1,21 @@
+# pragma once
+
+# include "common.h"
+
+# define INT_HASH_TABLE_SIZE 256
+
+typedef struct IntHashItem {
+	ListItem	list;
+	int			key;
+	int			value;
+} IntHashItem;
+
+typedef struct IntHashTable {
+	ListHead	table[INT_HASH_TABLE_SIZE];
+} IntHashTable;
+
+void	IntHashTable_init(IntHashTable *t);
+void	IntHashTable_clear(IntHashTable *t);
+void	IntHashTable_add(IntHashTable *t, int key, int value);
+int		*IntHashTable_get(IntHashTable *t, int key);
+void	IntHashTable_remove(IntHashTable *t, int key);
